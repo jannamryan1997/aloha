e44d5753b5fd169ab22a6496375be2e66ba6d0fd
Rado <rado.poghosyan@annaniks.com>
2020-03-10 15:54:58 +0400

    changed auth,login pages

--- src/app/com/annaniks/aloha/layout/index.ts
+++ src/app/com/annaniks/aloha/layout/index.ts
@@ -1,3 +1,3 @@
-export * from "../layout/footer/footer.component";
-export * from "../layout/header/header.component";
-export * from "../layout/loading/loading.component";
+export * from "./footer/footer.component";
+export * from "./header/header.component";
+export * from "./loading/loading.component";
