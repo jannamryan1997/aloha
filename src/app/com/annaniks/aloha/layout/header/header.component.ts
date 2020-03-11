6efee0a86b7a037134e5b3a12e67271d86ae8f5e
jannamryan1997 <jannamryan8@gmail.com>
2020-03-11 10:56:40 +0400

    creaded coupon content

--- src/app/com/annaniks/aloha/layout/header/header.component.ts
+++ src/app/com/annaniks/aloha/layout/header/header.component.ts
@@ -7,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HeaderComponent implements OnInit {
-
+    public coupon: boolean = false;
     constructor() { }
 
     ngOnInit() { }
+
+    private _openCouponMenu(): void {
+        this.coupon = !this.coupon;
+    }
+
+    public onclick(): void {
+        this._openCouponMenu();
+    }
+    public copyInputMessage(userinput): void {
+        userinput.select();
+        document.execCommand('copy');
+        userinput.setSelectionRange(0, 0);
+    }
 }
