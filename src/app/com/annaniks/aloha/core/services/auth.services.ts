a23acaa4cc053856a84a1c3cab16b2a2870fa8c4
jannamryan1997 <jannamryan8@gmail.com>
2020-04-10 15:36:44 +0400

    changed all bugs and buid message

--- src/app/com/annaniks/aloha/core/services/auth.services.ts
+++ src/app/com/annaniks/aloha/core/services/auth.services.ts
@@ -1,12 +1,13 @@
 import { Injectable } from "@angular/core";
 import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from '../models/profile';
+import { User, Country } from '../models/profile';
 
 @Injectable()
 
 export class AuthService {
     public user: User;
+    public count :Country[];
     constructor(private _httpClient: HttpClient) { }
 
     public getProfile(): Observable<User> {
